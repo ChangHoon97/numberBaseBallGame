cdbc76aab9975e3b3ae06f964f6a5eb8613e2716
Hoon <hyo04044@naver.com>
2023-10-27 08:23:34 +0900

    KAN-2 jyj test commit

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,7 @@ public class Main {
         System.out.println("KAN-1-Lee");
         System.out.println("test2");
         System.out.println("test3");
+        System.out.println("test4");
 
     }
 }
